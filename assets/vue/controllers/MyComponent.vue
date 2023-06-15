d0c24c483dec5b9a28d9004c40bc8e4cefbab0df
man razak <mandimby.razakanaivo@gmail.com>
2023-06-15 20:18:28 +0200

    ADD all

---
+++ assets/vue/controllers/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="test">Hello {{ name }}!</div>
+    <ul>
+        <li v-for="(notification, index) in notifications" :key="index">{{ notification }}</li>
+    </ul>
+</template>
+
+<script setup>
+    defineProps({
+        name: String,
+        notifications: Array,
+    });
+</script>
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'name' => 'DJ bobo',
+            'notifications' => ['abba', 'bobo', 'baba'],
+        ]);
+    }
+}
